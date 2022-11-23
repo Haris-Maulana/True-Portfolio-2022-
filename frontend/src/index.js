23eb004dcedda237e05d482b086e4c63ae54afbb
unknown <harismaulanaslm@gmail.com>
2022-11-23 21:03:29 +0700

    Progress

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home"
+import Layout from "./pages/Layout";
+import Project from "./pages/Project";
+import Feature from './pages/Feature';
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //<React.StrictMode>
-    <App />
-  //</React.StrictMode>
+  <>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      <Route path="project" element={<Project/>}/>
+      <Route path="feature" element={<Feature/>}/>
+      <Route path="dashboard" element={<Dashboard/>}/>
+    </Route>
+  </Routes>
+  </BrowserRouter>
+  </>
 );
